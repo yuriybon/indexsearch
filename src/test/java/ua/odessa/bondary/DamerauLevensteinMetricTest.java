0eaeb0a2296c7706d1c2b8a88456c8e502ae8629
yuriybon <yuriy_bon@mail.ru>
2018-10-08 18:49:05 +0200

    Add one more case

--- src/test/java/ua/odessa/bondary/DamerauLevensteinMetricTest.java
+++ src/test/java/ua/odessa/bondary/DamerauLevensteinMetricTest.java
@@ -13,6 +13,8 @@ public class DamerauLevensteinMetricTest {
         org.junit.Assert.assertEquals(1, metric.getDistance("AAA","AAB"));
         System.out.println(metric.getDistance("abbott","advance"));
         System.out.println(metric.getDistance("acterna","advance"));
+        //abercrombie advantica
+        System.out.println(metric.getDistance("abercrombie","advantica"));
 //        org.junit.Assert.assertEquals(1, metric.getDistance("abbott","advance"));
         // dictionary[index]=="abbott"
 
